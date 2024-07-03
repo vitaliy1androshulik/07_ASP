4d5a6f49e82891ff97883a835029ee669c889689
dev <dev@A32PC9>
2024-07-03 16:51:39 +0300

    Add project files.

---
+++ 01_WelcomeTOASP/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using _01_WelcomeTOASP.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _01_WelcomeTOASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult SecondButton()
+        {
+            return View();
+        }
+
+        public IActionResult ThirdButton()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 01_WelcomeTOASP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _01_WelcomeTOASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
